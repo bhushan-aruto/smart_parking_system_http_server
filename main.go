91cec5a804eda311d4de68458fb6c57ad68c4f30
Nagabhushan <nagabhushanbhandary524@gmail.com>
2024-11-20 15:21:07 +0530

    disabled the .env loader code

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
 	"github.com/bhushan-aruto/db"
 	"github.com/bhushan-aruto/repository"
 	"github.com/bhushan-aruto/route"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error occurred while loading the environment variables, Error -> %v\n", err.Error())
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatalf("error occurred while loading the environment variables, Error -> %v\n", err.Error())
+	// }
 
 	db, err := db.Connect()
 
